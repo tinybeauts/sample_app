42f2b4757b4fc2c08f877437e4403673ffcd5301
Julianne <jubeaudoin@gmail.com>
2012-07-24 04:18:03 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 				  "Julianne Beaudoin"
+		email 				  "jbeaudoin@example.com"
+		password 			  "foobar"
+		password_confirmation "foobar"
+	end
+end
